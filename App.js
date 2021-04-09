26996b7baa48155d3735e72fc178a2e44f344eb6
Aura Ulloa-Ordonez <auraulloa.9000@gmail.com>
2021-04-09 18:08:31 -0500

    can filter courses now

--- App.js
+++ App.js
@@ -33,6 +33,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
     paddingTop: 20,
   },
   bannerStyle: {
--- components/Course.js
+++ components/Course.js
@@ -5,25 +5,39 @@ const getCourseNumber = course => (
     course.id.slice(1)
 );
   
-const Course = ({course}) => (
-<TouchableOpacity style={styles.courseButton}>
+const Course = ({course, isDisabled, isSelected, select}) => (
+<TouchableOpacity style={styles[isSelected ? 'courseButtonSelected' : isDisabled ? 'courseButtonDisabled' :'courseButton']}
+    onPress={ () => { if (!isDisabled) select(course); }}>
     <Text style = {styles.courseText}>
     {`CS ${getCourseNumber(course)}\n${course.meets}`}
     </Text>
 </TouchableOpacity>
 );
 
+const courseButtonRef = {
+    flex: 1,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    height: 60,
+    padding: 10,
+    minWidth: 90,
+    maxWidth: 90, 
+};
+
 const styles = StyleSheet.create({
     courseButton: {
-      borderRadius: 5,
-      justifyContent: 'center',
-      alignItems: 'center',
-      margin: 10,
-      height: 60,
-      padding: 10,
-      minWidth: 90,
-      maxWidth: 90,
+      ...courseButtonRef,
       backgroundColor: '#66b0ff',
+    },
+    courseButtonSelected: {
+        ...courseButtonRef,
+        backgroundColor: '#004a99',
+    },
+    courseButtonDisable: {
+        ...courseButtonRef,
+        backgroundColor: '#d3d3d3',
     },
     courseText: {
       color: '#fff',
